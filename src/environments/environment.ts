09cbd3b6238d3dfc98b9769e737247d1b89b183b
SUKHAD SHARMA <mykeyman777@gmail.com>
2025-06-09 18:40:48 +0530

    [MM - v1] - Removing environment.ts from gitignore To trigger successfully deploy on netlify Still, keeping environment sample. -- original environment files to be removed later.

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCwo9gm4Re1QzVV4potC6w2nYclTLWbvQ0',
+    authDomain: 'moonlit-musings.firebaseapp.com',
+    databaseURL:
+      'https://moonlit-musings-default-rtdb.asia-southeast1.firebasedatabase.app/',
+    projectId: 'moonlit-musings',
+    storageBucket: 'moonlit-musings.firebasestorage.app',
+    messagingSenderId: '762066955991',
+    appId: '1:762066955991:web:2e90b357828370fbecca92',
+    measurementId: 'G-T4KNKFRBPT',
+  },
+};
