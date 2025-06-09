1144308b92b7dfe7d11b28adde5ef72937f62364
SUKHAD SHARMA <mykeyman777@gmail.com>
2025-06-10 01:39:28 +0530

    [MM - v1.1] - Updated the about me, navbar and the footer section. -- still in progress

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -23,4 +23,8 @@ export class HomeComponent {
       queryParams: { genre: genre.toLowerCase() },
     });
   }
+
+  goToInstagram() {
+    window.open('https://www.instagram.com/__.skull_candy.__/', '_blank');
+  }
 }
